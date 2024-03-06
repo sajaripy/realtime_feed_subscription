e13e5beb31d5d372d1350b475e4c9ce2a7bc557d
sajari <sajari.k.14@gmail.com>
2024-03-07 04:32:17 +0530

    last update

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # realtime_feed_subscription
  realtime feed subscription
+
+Implemented features:
+1. User Authentication: Users can register and log in and logout from the application.
+
+2. Subscription: Upon logging in, users can subscribe to a channel group to receive live feed updates. Subscribed user's info gets stored in UserSubscription model thus in feed_subscription_usersubscription table.
+
+3. WebSocket Integration: Used Daphne, which is the ASGI server used by Django Channels to handle WebSocket connections and send live feed messages to subscribed users.
+
+4. Scalability: Implemented throttling to handle large number of concurrent users
+subscribing to the feed. for ex: Page will redirect if refreshed for 10 times in a minute and will wait for a minute to go back when refreshed.
+
+To run the project:
+=> Move to the realtime_feed_subscription project directory containing manage.py
+=> activate pipenv using command: pipenv shell
+=> Install dependencies: 1. pip3 install django
+                      2. pip3 install djangorestframework
+                      3. pip3 install channels
+                      4. pip3 install websocket-client
+                      5. python -m pip install -U channels["daphne"]
+
+If you want to use Channels only and not Daphne:
+pip3 install channels==3.0.4
+pip3 install django==4.0.0
+These two versions are compatible with each other
+
+=> run migrations: python manage.py makemigrations
+                python manage.py migrate
+
+=> run the server: python manage.py runserver
+
+=> Use credentials if you don't want to register: username: sajari2 
+                                               password: Sasa@1499
+
+=> Click on subscribe to receive the live feed data.
--- feed_subscription/views.py
+++ feed_subscription/views.py
@@ -15,7 +15,7 @@ from .models import UserSubscription
 from .serializers import UserSubscriptionSerializer
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .services import start_binance_websocket
+# from .services import start_binance_websocket
 
 @login_required
 def home(request):
@@ -35,13 +35,6 @@ class UserSubscriptionViewSet(viewsets.ModelViewSet):
     queryset = UserSubscription.objects.all()
     serializer_class = UserSubscriptionSerializer
 
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import UserSubscription
-
-
 class SubscribeChannel(APIView):
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     def get(self, request):
@@ -56,49 +49,3 @@ class SubscribeChannel(APIView):
         subscription = UserSubscription(user=user)
         subscription.save()
         return render(request, 'subscribe.html', {})
-        # return Response({"message": f"Subscribed to channel successfully"}, status=status.HTTP_200_OK)
-
-        # Validate the channel_id and perform subscription logic
-        # if channel_id:
-        #     # Check if the user is already subscribed to the channel
-        #     if UserSubscription.objects.filter(user=user, channel_id=channel_id).exists():
-        #         return Response({"error": "User is already subscribed to this channel"}, status=status.HTTP_400_BAD_REQUEST)
-
-        #     # Create a new UserSubscription instance and save it to the database
-        #     subscription = UserSubscription(user=user, channel_id=channel_id)
-        #     subscription.save()
-
-        #     return Response({"message": f"Subscribed to channel {channel_id} successfully"}, status=status.HTTP_200_OK)
-        # else:
-        #     return Response({"error": "Invalid channel_id provided"}, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view()
-# @permission_classes([IsAuthenticated])
-# @throttle_classes([UserRateThrottle])
-# def throttle_check_auth(request):
-#     return Response({"message": "message for the logged in users only"})
-
-# Define an API view for receiving feed updates
-class FeedUpdates(APIView):
-    def post(self, request):
-        # Assuming you expect JSON data with feed update information in the request body
-        feed_data = request.data.get('feed_data')
-
-        # Your logic to handle the received feed updates
-        # You might process the updates, save them to the database, or perform any other actions
-        # For demonstration purposes, let's assume you simply return a success message
-
-        # Check if feed_data is provided
-        if feed_data:
-            # Process the feed data and perform update logic here
-            # For example, you could save the feed updates to the database
-            # Or distribute the updates to subscribed users
-            # For demonstration, we'll just print the received feed data
-            print("Received feed update:", feed_data)
-
-            return Response({"message": "Feed update received successfully"}, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "No feed data provided"}, status=status.HTTP_400_BAD_REQUEST)
-        # Handle POST request to receive feed updates
-        # Your feed update logic goes here
-        # return Response({"message": "Feed update received"}, status=status.HTTP_200_OK)
